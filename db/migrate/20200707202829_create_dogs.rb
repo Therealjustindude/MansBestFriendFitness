3a7524471aae1349ee40ed4955e5894f40042f9a
therealjustindude <therealjustindude@gmail.com>
2020-07-07 15:38:09 -0500

    resources for dog and user created

---
+++ db/migrate/20200707202829_create_dogs.rb
@@ -0,0 +1,16 @@
+class CreateDogs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+      t.integer :age
+      t.string :gender
+      t.integer :weight
+      t.boolean :intact
+      t.date :birthday
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
