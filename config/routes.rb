80cb2e04b40d182606983165172d84d4faf578d2
therealjustindude <therealjustindude@gmail.com>
2020-07-07 19:32:18 -0500

    User and site controller created, userhelper module created and user views file created, resource routes for users created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
   resources :dogs
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "user#index"
+  root "site#index"
 end
