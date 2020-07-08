b18fa2d3e069426b3ac1ed8155766ea6893037dd
therealjustindude <therealjustindude@gmail.com>
2020-07-07 21:58:59 -0500

    account user devise created and re migrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :accounts
+  devise_for :account_users
   resources :dogs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "site#index"
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_024553) do
+ActiveRecord::Schema.define(version: 2020_07_08_025548) do
 
-  create_table "accounts", force: :cascade do |t|
+  create_table "account_users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(version: 2020_07_08_024553) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_accounts_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_account_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_account_users_on_reset_password_token", unique: true
   end
 
   create_table "diet_entries", force: :cascade do |t|
