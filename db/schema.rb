6eeda17a983faf931fe938f4682a19c5f9ca5206
therealjustindude <therealjustindude@gmail.com>
2020-07-07 22:12:48 -0500

    devise User created

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_025548) do
-
+ActiveRecord::Schema.define(version: 2020_07_08_031036) do
 
   create_table "diet_entries", force: :cascade do |t|
     t.string "food"
@@ -53,6 +52,20 @@ ActiveRecord::Schema.define(version: 2020_07_08_025548) do
     t.integer "exercise_entry_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
