b81862b58bdfa825e2706358bd35b8cdb1514048
therealjustindude <therealjustindude@gmail.com>
2020-07-10 12:32:16 -0500

    omniauthable added to devis authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
+
   has_many :dogs
   has_many :journal_entries
+  
   def full_name
     self.first_name.capitalize + " " + self.last_name.capitalize 
   end
+
 end
