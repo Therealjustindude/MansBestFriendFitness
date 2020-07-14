025de2b17c65463c21c6d3ae65339a357b736c4d
therealjustindude <therealjustindude@gmail.com>
2020-07-14 14:15:34 -0500

    took omniauth out until ready to configure

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and 
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable
+         :recoverable, :rememberable, :validatable 
 
   has_many :dogs
   has_many :journal_entries
