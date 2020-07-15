3e3c7108c3ea3abc7f2072e8292b2effa08f5e57
therealjustindude <therealjustindude@gmail.com>
2020-07-14 20:11:32 -0500

    user strong params accepts dog_attributes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,10 @@ class UsersController < ApplicationController
     def show
         @user = User.find_by(id: params[:id])
     end 
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, dog_attributes[:name, :breed, :age, :gender, :weight, :intact, :birthday])
+    end
 end
