4d8125141e9f91ad24b90ed946accace0840cfd3
therealjustindude <therealjustindude@gmail.com>
2020-07-14 20:11:49 -0500

    dog attributes method created

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ApplicationRecord
 
   has_many :dogs
   has_many :journal_entries
+
+  def dog_attributes=(dog)
+    self.dog = Dog.find_by(id: dog[:id])
+    self.dog.update(dog)
+  end
   
   def full_name
     self.first_name.capitalize + " " + self.last_name.capitalize 
