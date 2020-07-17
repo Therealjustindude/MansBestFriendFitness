d30516f74a2a0cb106548b342089e8e1b45f7da8
therealjustindude <therealjustindude@gmail.com>
2020-07-17 16:49:10 -0500

    added omniauth callbacks controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   
   root "site#index"
   
-  devise_for :users, path: "accounts"
+  devise_for :users,controllers: { omniauth_callbacks: 'omniauth_callbacks' }, path: "accounts"
 
   resources :users, only: [:index, :show] do
     resources :dogs
   end
+
   
   
 end
