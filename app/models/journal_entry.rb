a85edd3c9411d981d828e2b688ba67e8e20750ae
therealjustindude <therealjustindude@gmail.com>
2020-07-17 20:04:00 -0500

    has many exercise and diet entries

--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -1,6 +1,6 @@
 class JournalEntry < ApplicationRecord
     belongs_to :user
     belongs_to :dog
-    belongs_to :exercise_entry, optional: true
-    belongs_to :diet_entry, optional: true
+    has_many :exercise_entries
+    has_many :diet_entries
 end
