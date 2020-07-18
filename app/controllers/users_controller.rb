d017600fe12784fd96b3ac82c8e28661bf9238b5
therealjustindude <therealjustindude@gmail.com>
2020-07-17 20:02:31 -0500

    user params update for diet,exercise and journal entries

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:first_name, :last_name, :email, dog_attributes[:name, :breed, :age, :gender, :weight, :intact, :birthday])
+        params.require(:user).permit(:first_name, :last_name, :email, dog_attributes[:name, :breed, :age, :gender, :weight, :intact, :birthday], journal_entry_attributes[:title, :entry,:date], diet_entry_attributes[:food, :snacks, :water, :calories], exercise_entry_attributes[:name, :duration, :description])
     end
 end
