49ac9362c42457bb576c07b193b491b640ae4fd8
therealjustindude <therealjustindude@gmail.com>
2020-07-18 22:24:22 -0500

    added load and authorize method for cancancan

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
+    load_and_authorize_resource
     before_action :authenticate_user!
     
-    def show
-        @user = User.find_by(id: params[:id])
+    def show  
+    
+         @user = User.find_by(id: params[:id])
+        
+
     end 
 
     private
