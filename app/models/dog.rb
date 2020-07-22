42deb0630652763af2321bf81444142e12a5084b
therealjustindude <therealjustindude@gmail.com>
2020-07-22 14:25:12 -0500

    unneccesary methods removed. validations added for dog sttr

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,28 +4,11 @@ class Dog < ApplicationRecord
     has_many :exercise_entries, through: :journal_entries
     has_many :diet_entries, through: :journal_entries
 
-    def journal_entry_attributes=(journal_entry_attributes)
-        self.journal_entry = JournalEntry.where(title: journal_entry_attributes[:title], user_id: journal_entry_attributes[:user_id]).first_or_create do |journal_entry|
-          journal_entry.title= journal_entry_attributes[:title]
-          journal_entry.entry= journal_entry_attributes[:entry]
-          journal_entry.date= journal_entry_attributes[:date]
-        end
-    end
+    validates :user_id, presence: true
+    validates :name, uniqueness: true, presence: true
+    validates :breed, presence: true
+    validates :age, presence: true
+    validates :gender, presence: true
     
-    def diet_entry_attributes=(diet_entry_attributes)
-        self.diet_entry = DietEntry.where(journal_entry_id: diet_entry_attributes[:journal_entry_id]).first_or_create do |diet_entry|
-          diet_entry.food= diet_entry_attributes[:food]
-          diet_entry.snacks=diet_entry_attributes[:snacks]
-          diet_entry.water= diet_entry_attributes[:water]
-          diet_entry.calories= diet_entry_attributes[:calories]
-        end
-    end
-    
-    def exercise_entry_attributes=(exercise_entry_attributes)
-        self.exercise_entry = ExerciseEntry.where(journal_entry_id: exercise_entry_attributes[:journal_entry_id]).first_or_create do |exercise_entry|
-          exercise_entry.name= exercise_entry_attributes[:name]
-          exercise_entry.duration=exercise_entry_attributes[:duration]
-          exercise_entry.description= exercise_entry_attributes[:description]
-        end
-    end
+
 end
