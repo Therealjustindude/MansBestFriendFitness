7f40cc717a77711722666c7c622e7b5215b24bb6
therealjustindude <therealjustindude@gmail.com>
2020-07-22 14:26:37 -0500

    show updated, new updated and using associations for form_for, create updated, strong params updated

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -1,31 +1,37 @@
 class JournalEntriesController < ApplicationController
     load_and_authorize_resource
     before_action :authenticate_user!
+    
 
     def show
-        if params[:user_id]
             @user = User.find_by(id: params[:user_id])
-            @dog = Dog.find_by(id: params[:dog_id])
             @journal_entry = @user.journal_entries.find_by(id: params[:id])
-            if @user.nil?
-              redirect_to user_journal_entries(@user), alert: "Journal entry not found"
-            end
-        else
+            if @journal_entry.nil?
+              redirect_to user_path(@user), alert: "Journal entry not found"
+            else
             @journal_entry = JournalEntry.find(params[:id])
-        end
+            end
     end
 
     def new
-        @user = User.find_by(id: params[:user_id])
-        @user_dog = @user.dogs.find_by(id: params[:dog_id])
-        @journal_entry = JournalEntry.new
+        
+        @user_dog = current_user.dogs.find_by(id: params[:dog_id])
+    
+        @new_je = current_user.journal_entries.build(dog_id: @user_dog.id)
+
+        @new_je.diet_entries.build
+        @new_je.exercise_entries.build
+        #should I be building associations ^^^
     end
 
     def create
-        @user = User.find_by(id: params[:user_id])
-        @user_dog = @user.dogs.find_by(id: params[:dog_id])
+        
+        @user_dog = current_user.dogs.find_by(id: params[:dog_id])
+
         @journal_entry = @user_dog.journal_entries.build(journal_entry_params)
         @journal_entry.user = current_user
+
+
             if @journal_entry.save
                 redirect_to user_path(current_user)
             else
@@ -54,6 +60,7 @@ class JournalEntriesController < ApplicationController
     private
 
     def journal_entry_params
-        params.require(:journal_entry).permit(:title, :entry,:date, diet_entries: [:food, :snacks, :water, :calories], exercise_entries: [:name, :duration, :description])
+        params.require(:journal_entry).permit(:title, :entry,:date, diet_entries_attributes: [:food, :snacks, :water, :calories], exercise_entries_attributes: [:name, :duration, :description])
+        #should it be exercise_entries and diet_entries
     end
 end
