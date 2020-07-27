de1a75fa8b34cef6669158568e15e67f682930c5
therealjustindude <therealjustindude@gmail.com>
2020-07-26 19:46:30 -0500

    added foreign key to haas many dogs

--- app/models/exercise_entry.rb
+++ app/models/exercise_entry.rb
@@ -1,4 +1,4 @@
 class ExerciseEntry < ApplicationRecord
     belongs_to :journal_entries, optional: true
-    has_many :dogs, through: :journal_entries, foreign_key "dog_id" 
+    has_many :dogs, through: :journal_entries, foreign_key: "dog_id" 
 end
