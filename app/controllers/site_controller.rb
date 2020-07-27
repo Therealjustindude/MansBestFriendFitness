87e78bbf1d7dadaa2fa6e36fbf43ddfa3ef109a0
therealjustindude <therealjustindude@gmail.com>
2020-07-26 19:48:16 -0500

    added query to index action

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,7 +1,10 @@
 class SiteController < ApplicationController
     before_action :authenticate_user!
     def index
-        @users = User.all 
-        
+        if params[:query]
+            @user= User.where('FIRST_NAME like ?', "%#{params[:query]}%")
+        else
+            @user = User.all
+        end
     end
 end
