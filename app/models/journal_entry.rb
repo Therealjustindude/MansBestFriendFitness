a720e6c0fbf061834502fbca5a4044d7869beac5
therealjustindude <therealjustindude@gmail.com>
2020-07-26 19:46:02 -0500

    diet and exercise entries attributes can create and update instances accordingly

--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -11,32 +11,40 @@ class JournalEntry < ApplicationRecord
     validates :date, presence:true
 
     
-      def diet_entries_attributes=(diet_info)
-        diet_obj = DietEntry.where(journal_entry_id: self.id).first_or_create do | diet_entry|
-            diet_info.values.each do|info|
 
-                diet_entry.food= info[:food]
-                diet_entry.snacks=info[:snacks]
-                diet_entry.water= info[:water]
-                diet_entry.calories= info[:calories]
+      def diet_entries_attributes=(diet_info)
+        diet_obj= DietEntry.find_by(journal_entry_id: self.id)
+        if diet_obj.journal_entry_id.nil?
+          diet_obj = self.diet_entries.build
+            diet_info.values.each do|info| 
+                diet_obj.food= info[:food]
+                diet_obj.snacks=info[:snacks]
+                diet_obj.water= info[:water]
+                diet_obj.calories= info[:calories]
             end
+            self.diet_entries << diet_obj 
+        else
+          diet_params = diet_info.values[0]
+          diet_obj.update(diet_params)
         end
-        
-        self.diet_entries << diet_obj 
     
       end
     
       def exercise_entries_attributes=(exercise_info)
-        
-        exercise_obj = ExerciseEntry.where(journal_entry_id: self.id).first_or_create do |exercise_entry|
-            exercise_info.values.each do |info|
-                exercise_entry.name= info[:name]
-                exercise_entry.duration=info[:duration]
-                exercise_entry.description= info[:description]
-            end
-        end
-        self.exercise_entries <<  exercise_obj 
-        
+        exercise_obj= ExerciseEntry.find_by(journal_entry_id: self.id)
+
+        if exercise_obj.journal_entry_id.nil?
+          exercise_obj = self.exercise_entries.build 
+              exercise_info.values.each do |info|
+                  exercise_obj.name= info[:name]
+                  exercise_obj.duration=info[:duration]
+                  exercise_obj.description= info[:description]
+              end
+          self.exercise_entries <<  exercise_obj 
+        else 
+          exercise_params = exercise_info.values[0]
+          exercise_obj.update(exercise_params)
+        end 
       end
     
 end
