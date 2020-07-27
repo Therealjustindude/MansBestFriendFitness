1ac250138d2a98ed029732f80c5dea3044ef15e1
therealjustindude <therealjustindude@gmail.com>
2020-07-27 16:45:07 -0500

    private methods to help dry up code

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -5,65 +5,52 @@ class JournalEntriesController < ApplicationController
     
 
     def show
-        if current_user.id != params[:user_id]
+        if current_user.id = params[:user_id]
+            current_user_journal_entry
+        else 
             user = User.find_by(id: params[:user_id])
             @journal_entry = user.journal_entries.find_by(id: params[:id])
-
-
-        else 
-            @user_dog = current_user.dogs.find_by(id: params[:dog_id])
-            @journal_entry = @user_dog.journal_entries.find_by(id: params[:id])
         end
 
 
         if @journal_entry.nil?
-            user = User.find_by(id: parmas[:user_id])
-            redirect_to user_path(user), alert: "Journal entry not found"
+            redirect_to user_path(current_user), alert: "Journal entry not found"
         end
     end
 
     def new
-        
-        @user_dog = current_user.dogs.find_by(id: params[:dog_id])
-    
+        current_user_dog    
         @journal_entry = current_user.journal_entries.build(dog_id: @user_dog.id)
-
         @journal_entry.diet_entries.build
         @journal_entry.exercise_entries.build
-        #should I be building associations ^^^
     end
 
     def create
-        
-        @user_dog = current_user.dogs.find_by(id: params[:dog_id])
-
+        current_user_dog
         @journal_entry = @user_dog.journal_entries.build(journal_entry_params)
         @journal_entry.user = current_user
-
-
-            if @journal_entry.save
-                redirect_to user_path(current_user)
-            else
-                render :new
-            end 
+        if @journal_entry.save
+            redirect_to user_path(current_user)
+        else
+            render :new
+        end 
     end
 
     def edit
-        @user_dog = current_user.dogs.find_by(id: params[:dog_id])
-        @journal_entry = @user_dog.journal_entries.find_by(id: params[:id])
+        current_user_dog        
+        current_user_journal_entry    
     end
 
     def update
-        journal_entry = current_user.journal_entries.find_by(id: params[:id])
-
-        journal_entry.update(journal_entry_params)
+        current_user_journal_entry
+        @journal_entry.update(journal_entry_params)
         
         redirect_to user_path(current_user)
     end
 
     def destroy
-        journal_entry = current_user.journal_entries.find_by(id: params[:id])
-        journal_entry.destroy
+        current_user_journal_entry
+        @journal_entry.destroy
         redirect_to user_path(current_user)
     end
 
@@ -71,6 +58,13 @@ class JournalEntriesController < ApplicationController
 
     def journal_entry_params
         params.require(:journal_entry).permit(:title, :entry,:date, diet_entries_attributes: [:food, :snacks, :water, :calories], exercise_entries_attributes: [:name, :duration, :description])
+    end
     
+    def current_user_dog
+        @user_dog = current_user.dogs.find_by(id: params[:dog_id])
+    end
+
+    def current_user_journal_entry
+        @journal_entry = current_user.journal_entries.find_by(id: params[:id])
     end
 end
