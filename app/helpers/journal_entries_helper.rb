978e54105f4738309932813b7bf1eee709730127
therealjustindude <therealjustindude@gmail.com>
2020-07-27 19:03:06 -0500

    current user journal entry and dog helper methods added for controller

--- app/helpers/journal_entries_helper.rb
+++ app/helpers/journal_entries_helper.rb
@@ -1,2 +1,10 @@
 module JournalEntriesHelper
+
+    def current_user_journal_entry
+        @journal_entry = current_user.journal_entries.find_by(id: params[:id])
+    end
+
+    def current_user_dog
+        @user_dog = current_user.dogs.find_by(id: params[:dog_id])
+    end
 end
