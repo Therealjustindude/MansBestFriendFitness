d1d5531577eb8cd3da247a14730363f788cc07d1
therealjustindude <therealjustindude@gmail.com>
2020-07-27 19:03:51 -0500

    current user and not current_user_and_dog helper methods added for controller

--- app/helpers/dogs_helper.rb
+++ app/helpers/dogs_helper.rb
@@ -1,2 +1,11 @@
 module DogsHelper
+
+    def current_users_dog
+        @dog = current_user.dogs.find_by(id: params[:id])
+    end
+
+    def not_current_user_and_dog
+        @user = User.find_by(id: params[:user_id])
+        @dog = @user.dogs.find_by(id: params[:id])
+    end
 end
